ad91306e496d244abc1a7b624b37fea9c6413f6c
India Amos <india@indiamos.com>
2017-08-04 18:30:49 -0400

    FEAT(mvp): Define word model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,15 +2,19 @@ const Author = require('./author');
 const Book = require('./book');
 const Genre = require('./genre');
 const User = require('./user');
+const Word = require('./word');
 
 Author.hasMany(Book);
 Book.hasMany(Author);
 Book.hasMany(Genre);
+Book.hasMany(Word);
 Genre.hasMany(Book);
+Word.hasMany(Book);
 
 module.exports = {
   Author,
   Book,
   Genre,
-  User
+  User,
+  Word
 };
