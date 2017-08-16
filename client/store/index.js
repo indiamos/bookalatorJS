32c010e489b33f5fc07e89b46dadcf807fc295db
India Amos <india@indiamos.com>
2017-08-16 10:22:58 -0400

    Lint more of the things

--- client/store/index.js
+++ client/store/index.js
@@ -20,14 +20,14 @@
 //    }
 // }
 
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import book from './bookReducer';
 import user from './userReducer';
 
-const reducer = combineReducers({book, user});
-const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}));
+const reducer = combineReducers({ book, user });
+const middleware = applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }));
 const store = createStore(reducer, middleware);
 
 export default store;
--- server/api/authors.js
+++ server/api/authors.js
@@ -1,5 +1,6 @@
-const router = require('express').Router()
-const { Author, Book } = require('../db/models')
+const router = require('express').Router();
+const { Author, Book } = require('../db/models');
+
 module.exports = router;
 
 // GET      /api/authors/                   // returns all author objects
@@ -12,43 +13,43 @@ module.exports = router;
 // GET /api/authors/
 router.get('/', (req, res, next) => {
   Author.findAll()
-  .then(authors => res.json(authors))
-  .catch(next);
+    .then(authors => res.json(authors))
+    .catch(next);
 });
 
 // POST /api/authors/
 router.post('/', (req, res, next) => {
   Author.create(req.body)
-  .then(author => res.status(201).json(author))
-  .catch(next);
+    .then(author => res.status(201).json(author))
+    .catch(next);
 });
 
 // GET /api/authors/:authorId
 router.get('/:authorId', (req, res, next) => {
   Author.findById(req.params.authorId)
-  .then(author => res.json(author))
-  .catch(next);
+    .then(author => res.json(author))
+    .catch(next);
 });
 
 // PUT /api/authors/:authorId
 router.put('/:authorId', (req, res, next) => {
   Author.findById(req.params.authorId)
-  .then(author => author.update(req.body))
-  .then(updated => {
-    let revised = updated.dataValues; // putting this value directly in line 39 errors
-    res.send({ message: 'Updated sucessfully', revised })
-  })
-  .catch(next);
+    .then(author => author.update(req.body))
+    .then((updated) => {
+      const revised = updated.dataValues; // putting this value directly in line 39 errors
+      res.send({ message: 'Updated sucessfully', revised });
+    })
+    .catch(next);
 });
 
 // DELETE /api/authors/:authorId
 router.delete('/:authorId', (req, res, next) => {
   Author.findById(req.params.authorId)
-  .then(foundAuthor => foundAuthor.destroy())
-  .then(result => {
-    res.send({ message: 'Deleted successfully' })
-  })
-  .catch(next);
+    .then(foundAuthor => foundAuthor.destroy())
+    .then((result) => {
+      res.send({ message: 'Deleted successfully' });
+    })
+    .catch(next);
 });
 
 // GET /api/authors/:authorId/books
@@ -56,13 +57,14 @@ router.get('/:authorId/books', (req, res, next) => {
   Book.findAll({
     include: [
       {
-        model: Author, as: 'Creators',
+        model: Author,
+        as: 'Creators',
         where: {
-          id: req.params.authorId
-        }
-      }
-    ]
+          id: req.params.authorId,
+        },
+      },
+    ],
   })
-  .then(author => res.json(author))
-  .catch(next);
+    .then(author => res.json(author))
+    .catch(next);
 });
