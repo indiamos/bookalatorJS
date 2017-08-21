0213b3e88a8898430dd0d9f381287111bf73822e
India Amos <india@indiamos.com>
2017-08-21 00:09:50 -0400

    FORMAT: Slight linting in a an attempt to force remerge?

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {withRouter, Link} from 'react-router-dom';
-import {logout} from '../store';
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+import { logout } from '../store';
 import Navbar from './Navbar';
 
 /* ----------------------- COMPONENT -----------------------
@@ -12,31 +12,31 @@ import Navbar from './Navbar';
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props;
+  const { children, handleClick, isLoggedIn } = props;
 
   return (
     <div id="main">
-        <div>
-          <Navbar isLoggedIn={isLoggedIn} />
-          {children}
-        </div>
+      <div>
+        <Navbar isLoggedIn={isLoggedIn} />
+        {children}
+      </div>
     </div>
-  )
+  );
 };
 
 /* ----------------------- CONTAINER ----------------------- */
 
 const mapState = (state) => {
   return {
-    isLoggedIn: !!state.user.id
-  }
-}
+    isLoggedIn: !!state.user.id,
+  };
+};
 
 const mapDispatch = (dispatch) => {
   return {
     handleClick () {
       dispatch(logout());
-    }
+    },
   };
 };
 
@@ -47,7 +47,7 @@ export default withRouter(connect(mapState, mapDispatch)(Main));
 /* ----------------------- PROP TYPES ----------------------- */
 
 Main.propTypes = {
-  children: PropTypes.object,
+  children: PropTypes.shape(),
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+  isLoggedIn: PropTypes.bool.isRequired,
+};
