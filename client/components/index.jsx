a658c3bda6f5ae8c6fe005255521fc6ac986a444
India Amos <india@indiamos.com>
2017-08-21 00:23:02 -0400

    Reconcile components/index.jsx with master

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -3,7 +3,12 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Main} from './Main'
-export {default as Navbar} from './Navbar'
-export {default as UserHome} from './user-home'
-export {Login, Signup} from './auth-form'
+export { default as BookMetadata } from './BookMetadata';
+export { default as BookSelector } from './BookSelector';
+export { default as BookThumbnail } from './BookThumbnail';
+export { default as BookWordTable } from './BookWordTable';
+export { default as SingleBook } from './SingleBook';
+export { default as Main } from './Main';
+export { default as Navbar } from './Navbar';
+export { default as UserHome } from './user-home';
+export { Login, Signup } from './auth-form';
