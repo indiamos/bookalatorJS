2139a4bfb1c96cc50018ae8aba11ca307f7f708d
India Amos <india@indiamos.com>
2017-08-21 01:30:40 -0400

    FORMAT: Lint

--- client/components/auth-form.jsx
+++ client/components/auth-form.jsx
@@ -36,33 +36,27 @@ const AuthForm = (props) => {
  *   function, and share the same Component. This is a good example of how we
  *   can stay DRY with interfaces that are very similar to each other!
  */
-const mapLogin = (state) => {
-  return {
-    name: 'login',
-    displayName: 'Login',
-    error: state.user.error,
-  };
-};
+const mapLogin = state => ({
+  name: 'login',
+  displayName: 'Login',
+  error: state.user.error,
+});
 
-const mapSignup = (state) => {
-  return {
-    name: 'signup',
-    displayName: 'Sign Up',
-    error: state.user.error,
-  };
-};
+const mapSignup = state => ({
+  name: 'signup',
+  displayName: 'Sign Up',
+  error: state.user.error,
+});
 
-const mapDispatch = (dispatch) => {
-  return {
-    handleSubmit(evt) {
-      evt.preventDefault();
-      const formName = evt.target.name;
-      const email = evt.target.email.value;
-      const password = evt.target.password.value;
-      dispatch(auth(email, password, formName));
-    },
-  };
-};
+const mapDispatch = dispatch => ({
+  handleSubmit(evt) {
+    evt.preventDefault();
+    const formName = evt.target.name;
+    const email = evt.target.email.value;
+    const password = evt.target.password.value;
+    dispatch(auth(email, password, formName));
+  },
+});
 
 export const Login = connect(mapLogin, mapDispatch)(AuthForm);
 export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
@@ -72,5 +66,9 @@ AuthForm.propTypes = {
   name: PropTypes.string.isRequired,
   displayName: PropTypes.string.isRequired,
   handleSubmit: PropTypes.func.isRequired,
-  error: PropTypes.obj,
+  error: PropTypes.shape(),
+};
+
+AuthForm.defaultProps = {
+  error: {},
 };
