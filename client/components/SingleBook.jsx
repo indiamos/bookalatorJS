d7bf0bde493aab5b1056ec40e671214e2b4a6027
India Amos <india@indiamos.com>
2017-11-01 20:57:14 -0400

    FIX: Resize logo, reconnect dummy data
    The components are now rendering again with fake data. To be continued…

--- client/components/SingleBook.jsx
+++ client/components/SingleBook.jsx
@@ -25,7 +25,8 @@ const dummyBook = {
   author: dummyAuthor,
 };
 
-console.log('dummyBook:', dummyBook);
+// console.log('dummyBook:', dummyBook);
+// console.log('dummyAuthor:', dummyAuthor);
 // console.log('dummyBook.wordMap:', dummyBook.wordMap);
 
 const SingleBook = ({
@@ -33,41 +34,44 @@ const SingleBook = ({
   comparisonBook = dummyComparisonBook,
   handleChange = dummyHandleChange,
   singleBook = dummyBook,
-}) => (
-  <div className="single-book col-lg-8 col-lg-offset-2">
-    <div className="single-book row">
-      <div className="left-book-card col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <BookSelector
-          books={books}
-          onChange={handleChange}
-          selectedBook={singleBook.id}
-          selectorID="leftSelector"
-        />
-        <div className="book-thumb-meta row">
-          <BookThumbnail coverURL={singleBook.coverURL} title={singleBook.title} />
-          <BookMetadata singleBook={singleBook} />
+}) => {
+  console.log('singleBook:', singleBook);
+  return (
+    <div className="single-book col-lg-8 col-lg-offset-2">
+      <div className="single-book row">
+        <div className="left-book-card col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <BookSelector
+            books={books}
+            onChange={handleChange}
+            selectedBook={singleBook.id}
+            selectorID="leftSelector"
+          />
+          <div className="book-thumb-meta row">
+            <BookThumbnail coverURL={singleBook.coverURL} title={singleBook.title} />
+            <BookMetadata singleBook={singleBook} />
+          </div>
+        </div>
+
+        <div className="right-book-card col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <BookSelector
+            books={books}
+            onChange={handleChange}
+            selectedBook={comparisonBook.id}
+            selectorID="rightSelector"
+          />
         </div>
       </div>
 
-      <div className="right-book-card col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <BookSelector
-          books={books}
+      <div className="single-book row">
+        <BookWordTable
+          wordMap={singleBook.wordMap}
+          bookId={singleBook.id}
           onChange={handleChange}
-          selectedBook={comparisonBook.id}
-          selectorID="rightSelector"
         />
       </div>
     </div>
-
-    <div className="single-book row">
-      <BookWordTable
-        wordMap={singleBook.wordMap}
-        bookId={singleBook.id}
-        onChange={handleChange}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 SingleBook.propTypes = {
   books: PropTypes.arrayOf(
@@ -82,7 +86,19 @@ SingleBook.defaultProps = {
   books: [],
   comparisonBook: {},
   handleChange: () => console.log('handleChange was invoked'),
-  singleBook: {},
+  singleBook: {
+    id: 1,
+    title: 'Persuasion',
+    year: 1821,
+    wordCount: 83710,
+    uniqueCount: 6033,
+    coverURL: 'https://upload.wikimedia.org/wikipedia/commons/6/68/NorthangerPersuasionTitlePage.jpg',
+    pgURL: 'http://www.gutenberg.org/ebooks/105',
+    wikipediaURL: 'https://en.wikipedia.org/wiki/Persuasion_(novel)',
+    sentenceArray: persuasionSentenceArray,
+    wordMap: persuasionWordMap,
+    author: dummyAuthor,
+  },
 };
 
 export default SingleBook;
