3a0f34ee7eb62968860a719794002280431e4e6a
India Amos <india@indiamos.com>
2017-11-02 23:59:38 -0400

    FORMAT: Reorganize routes with `.route()`
    Also added `{ returning: true, plain: true }` to update routes, which were returning only counts.

--- server/api/authors.js
+++ server/api/authors.js
@@ -11,60 +11,43 @@ module.exports = router;
 // GET      /api/authors/:authorId/books    // returns all book objects by a given author
 
 // GET /api/authors/
-router.get('/', (req, res, next) => {
-  Author.findAll()
-    .then(authors => res.json(authors))
-    .catch(next);
-});
-
 // POST /api/authors/
-router.post('/', (req, res, next) => {
-  Author.create(req.body)
+router.route('/')
+  .get((req, res, next) => Author.findAll()
+    .then(authors => res.json(authors))
+    .catch(next))
+  .post((req, res, next) => Author.create(req.body)
     .then(author => res.status(201).json(author))
-    .catch(next);
-});
+    .catch(next));
 
 // GET /api/authors/:authorId
-router.get('/:authorId', (req, res, next) => {
-  Author.findById(req.params.authorId)
-    .then(author => res.json(author))
-    .catch(next);
-});
-
 // PUT /api/authors/:authorId
-router.put('/:authorId', (req, res, next) => {
-  Author.findById(req.params.authorId)
-    .then(author => author.update(req.body))
-    .then((updated) => {
-      const revised = updated.dataValues; // putting this value directly in line 39 errors
-      res.send({ message: 'Updated sucessfully', revised });
-    })
-    .catch(next);
-});
-
 // DELETE /api/authors/:authorId
-router.delete('/:authorId', (req, res, next) => {
-  Author.findById(req.params.authorId)
+router.route('/:authorId')
+  .get((req, res, next) => Author.findById(req.params.authorId)
+    .then(author => res.json(author))
+    .catch(next))
+  .put((req, res, next) => Author.findById(req.params.authorId)
+    .then(author => author.update(req.body, {
+      returning: true,
+      plain: true,
+    }))
+    .then(updated => res.send({ message: 'Updated sucessfully', updated }))
+    .catch(next))
+  .delete((req, res, next) => Author.findById(req.params.authorId)
     .then(foundAuthor => foundAuthor.destroy())
-    .then(() => {
-      res.send({ message: 'Deleted successfully' });
-    })
-    .catch(next);
-});
+    .then(() => res.send({ message: 'Deleted successfully' }))
+    .catch(next));
 
 // GET /api/authors/:authorId/books
-router.get('/:authorId/books', (req, res, next) => {
-  Book.findAll({
-    include: [
-      {
-        model: Author,
-        as: 'Creators',
-        where: {
-          id: req.params.authorId,
-        },
-      },
-    ],
-  })
-    .then(author => res.json(author))
-    .catch(next);
-});
+router.get('/:authorId/books', (req, res, next) => Book.findAll({
+  include: [{
+    model: Author,
+    as: 'Creators',
+    where: {
+      id: req.params.authorId,
+    },
+  }],
+})
+  .then(author => res.json(author))
+  .catch(next));
